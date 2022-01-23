66c931511447ccbd2aebcfbe683468e88e79ff6b
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-01-23 15:41:15 -0300

    added print_ip_oled.py

---
+++ code_for_jetson_nano/print_ip_oled.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+ip = sys.argv[1]
+print(ip)
+
+#OLED 128x64
+
+from luma.core.interface.serial import i2c, spi, pcf8574
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
+
+# rev.1 users set port=0
+# substitute spi(device=0, port=0) below if using that interface
+# substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
+serial = i2c(port=1, address=0x3C)
+
+# substitute ssd1331(...) or sh1106(...) below if using that device
+device = ssd1306(serial)
+
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((30, 40), ip , fill="white")
+
+#loop
+import time
+
+time.sleep(20)
