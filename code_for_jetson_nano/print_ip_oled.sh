51315fad09223b497a30bc239abb364eb93ec2cb
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-01-23 16:51:03 -0300

    new path for startup

--- code_for_jetson_nano/print_ip_oled.sh
+++ code_for_jetson_nano/print_ip_oled.sh
@@ -2,4 +2,4 @@
 ip="$(ifconfig | grep -A 1 'wlan0' | tail -1 | cut -d ':' -f 2 | tr ' ' '-')";
 ip=${ip:13:13}
 
-python3 print_ip_oled.py $ip
+python3 ~/FATEC_TCC_2022/code_for_jetson_nano/print_ip_oled.py $ip
