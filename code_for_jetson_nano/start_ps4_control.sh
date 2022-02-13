53b4869a21757ce95512bd57023cbba24eb845c9
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-02-12 22:16:15 -0300

    new file start_ps4_control.sh

---
+++ code_for_jetson_nano/start_ps4_control.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "_____press (SHARE + PS4) button on your controller_____"
+echo "_____press (SHARE + PS4) button on your controller_____"
+echo "_____press (SHARE + PS4) button on your controller_____"
+
+ds4drv
