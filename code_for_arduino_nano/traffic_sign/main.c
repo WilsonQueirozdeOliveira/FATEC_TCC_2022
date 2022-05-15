73ba5f0fa21e43a1f3479c12bbbe600e6aba5cae
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-05-15 15:53:27 -0300

    added ports LEDs control

--- code_for_arduino_nano/traffic_sign/main.c
+++ code_for_arduino_nano/traffic_sign/main.c
@@ -1,27 +1,31 @@
 /*
 
-red		= gnd	=
-black	= D2	= PORTD 2
-green	= D4	= PORTD 4
-yellow	= D6	= PORTD 6
+red	= gnd	=
+black	= D2	= PORTD 2 LED_PIN_R
+green	= D4	= PORTD 4 LED_PIN_Y
+yellow	= D6	= PORTD 6 LED_PIN_G
 
 */
 
 
 #define F_CPU 16000000UL
-#define LED_PIN_R 0
-#define LED_PIN_Y 0
-#define LED_PIN_G 0
+#define LED_PIN_R 2
+#define LED_PIN_Y 4
+#define LED_PIN_G 6
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
 {
-	DDRB |= (1<<LED_PIN_R);// set pin (PORTB 0) as output
+	DDRD |= (1<<LED_PIN_R);// set pin (PORTD 2) as output
+	DDRD |= (1<<LED_PIN_Y);// set pin (PORTD 4) as output
+	DDRD |= (1<<LED_PIN_G);// set pin (PORTD 6) as output
 	while(1)
 	{
-		PORTB ^= (1<<LED_PIN_R);// toggles pin (PORTB 0)
+		PORTD ^= (1<<LED_PIN_R);// toggles pin (PORTD 2)
+		PORTD ^= (1<<LED_PIN_Y);// toggles pin (PORTD 4)
+		PORTD ^= (1<<LED_PIN_G);// toggles pin (PORTD 6)
 		_delay_ms(1000); // busy wait, 500ms
 	}
 }
