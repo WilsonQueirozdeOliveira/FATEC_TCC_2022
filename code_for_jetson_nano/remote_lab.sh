576fa3fec99d341f4407ae160064da45d315c564
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-05-28 20:27:20 +0200

    added remote_lab.sh file for remote jupyter lad

---
+++ code_for_jetson_nano/remote_lab.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ip="$(ifconfig | grep -A 1 'wlan0' | tail -1 | cut -d ':' -f 2 | tr ' ' '-')";
+ip=${ip:13:13}
+echo "$ip"
+sudo chmod 666 /dev/ttyTHS1
+jupyter lab --ip "$ip" --port 8888
+
